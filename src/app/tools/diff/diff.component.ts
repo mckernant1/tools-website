5c04a44c033df45a9898a4d14f33ecde5033a9fd
Tom McKernan <30330917+mckernant1@users.noreply.github.com>
2021-06-02 18:54:44 -0700

    adding material

---
+++ src/app/tools/diff/diff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diff',
+  templateUrl: './diff.component.html',
+  styleUrls: ['./diff.component.less']
+})
+export class DiffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/tools/tools.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DiffComponent } from './diff/diff.component';
+
+
+
+@NgModule({
+  declarations: [
+    DiffComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ToolsModule { }
